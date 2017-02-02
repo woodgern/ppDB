5c2b831690555f51eef73cd3d129ffdd786f7459
woodgern <woodgern@gmail.com>
2017-02-01 22:39:52 -0800

    website code fixed, should work with API design

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -3,22 +3,19 @@
     	var url = 'http://192.168.1.108:3000/pp';
     	$.ajax(url, {
             dataType: 'json',
-            headers: {
-
-            },
-            success: function(r) {
+            success: function(data) {
             	$("#pp").html(data.word);
-                window.alert(JSON.stringify(r));
             },
             error: function(r) {
-                window.alert(JSON.stringify(r));
+                window.alert('error');
             }
         });
+
         /*var x = $.get("http://192.168.1.108:3000/pp", function(data) {
         	window.alert(data);
             $("#pp").html(data.word);
-        }).fail(function() {
-        	window.alert('failed');
+        }).fail(function(r) {
+        	window.alert(JSON.stringify(r));
         });*/
     }
 })(window);
