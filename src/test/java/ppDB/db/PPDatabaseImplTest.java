9166fb04d3eb36348e7ec53c13dafae8b24548b0
HiPERnx <gustaf.nilstadius@telia.com>
2017-02-01 22:00:44 -0800

    Added test

--- src/test/java/ppDB/db/PPDatabaseImplTest.java
+++ src/test/java/ppDB/db/PPDatabaseImplTest.java
@@ -1,8 +1,20 @@
 package ppDB.db;
 
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.asyncsql.AsyncSQLClient;
+import io.vertx.ext.asyncsql.MySQLClient;
+import io.vertx.ext.sql.SQLConnection;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
@@ -10,20 +22,45 @@ import static org.junit.Assert.*;
  * @author Gustaf Nilstadius
  *         Created by Gustaf Nilstadius ( hipernx ) on 2017-02-01.
  */
+@RunWith(VertxUnitRunner.class)
 public class PPDatabaseImplTest {
-    @Before
-    public void setUp() throws Exception {
 
+    private AsyncSQLClient asyncSQLClient;
+    private PPDatabase ppDatabase;
+    private Vertx vertx = Vertx.vertx();
+
+    @Before
+    public void setUp(TestContext context) throws Exception {
+        asyncSQLClient = MySQLClient.createShared(vertx, getSQLSettings());
+        ppDatabase = new PPDatabaseImpl();
     }
 
     @After
-    public void tearDown() throws Exception {
-
+    public void tearDown(TestContext context) throws Exception {
+        asyncSQLClient.close();
+        vertx.close();
     }
 
     @Test
-    public void getRandomWord() throws Exception {
-
+    public void getRandomWord(TestContext context) throws Exception {
+        Async async = context.async();
+        Future<JsonObject> future = Future.future();
+        ppDatabase.getRandomWord(asyncSQLClient, future.setHandler(res -> {
+            context.assertTrue(res.succeeded());
+            async.complete();
+        }));
+        async.awaitSuccess(10000L);
     }
+
+    private JsonObject getSQLSettings () {
+        return new JsonObject()
+                .put("host", "localhost")
+                .put("port", 3306)
+                .put("maxPoolSize", 10)
+                .put("username", "ppdb")
+                .put("password", "qwerty")
+                .put("database", "pp_db");
+    }
+
 
 }
