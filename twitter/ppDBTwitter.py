b178850194d8d602ebf45174183caa1229af4a1f
woodgern <woodgern@gmail.com>
2017-02-06 23:04:43 -0800

    Twitter bot running up to API call

--- twitter/ppDBTwitter.py
+++ twitter/ppDBTwitter.py
@@ -1,4 +1,11 @@
 import tweepy, time, sys
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import requests
+import json
+url = 'http://FILLINHERE'
+data = '{}'
  
 CONSUMER_KEY = 'M6gCybQsVWx92siwhrupVzwRN'
 CONSUMER_SECRET = '11AQJVDi8MCmgtWxuxhPXNlDxfng8scETuqL1N8c5dNndGXP1a'
@@ -6,10 +13,32 @@ ACCESS_KEY = '827411483487133697-t4No89IgY4RLLhroEgslBZ343qEiqiO'
 ACCESS_SECRET = 'qtZjuSZ9l58nCXOD2RpWUbAhG1SsWme14q59CEWD9Vi8u'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
 api = tweepy.API(auth)
 
-mentions = api.mentions_timeline()
+class StdOutListener(StreamListener):
 
-for mention in mentions:
-    print mention.text
-    print mention.user.screen_name
+    def on_data(self, data):
+        data = json.loads(data)
+        print data['text']
+        target = ""
+        for mention in data['entities']['user_mentions']:
+        	if target == "" and mention['screen_name'] != "ppdatabase":
+        		target = mention['screen_name']
+        		break
+        #response = requests.post(url, data=data)
+        api.update_status("@" + target + " johnson")
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['@ppdatabase'])
